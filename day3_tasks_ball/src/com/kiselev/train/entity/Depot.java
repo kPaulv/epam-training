504e32dbea49b29ec4bb13c7535d288fb318d80b
kPaulv <74295783+kPaulv@users.noreply.github.com>
2020-11-16 12:11:03 +0300

    Update Depot.java
    Service methods removed.

--- day3_tasks_ball/src/com/kiselev/train/entity/Depot.java
+++ day3_tasks_ball/src/com/kiselev/train/entity/Depot.java
@@ -1,6 +1,5 @@
 package com.kiselev.train.entity;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class Depot {
@@ -14,82 +13,19 @@ public class Depot {
         this.trains = trains;
     }
 
-    public Train[] similarDestination(String destination) {
-        Train[] similarDestTrains = new Train[trains.length];
-        int count = 0;
-        for(int i = 0; i < trains.length; i++) {
-            if(trains[i].getDestination().equalsIgnoreCase(destination)){
-                similarDestTrains[i] = trains[i];
-                count++;
-            }
-        }
-        similarDestTrains = Arrays.copyOf(similarDestTrains, count);
-        return similarDestTrains;
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Depot{");
+        sb.append("trains=").append(Arrays.toString(trains));
+        sb.append('}');
+        return sb.toString();
     }
 
-    public Train[] similarDestinationTime(String destination, LocalDateTime time) {
-        Train[] similarDestTime = new Train[trains.length];
-        int count = 0;
-        for(int i = 0; i < trains.length; i++) {
-            if(trains[i].getDestination().equalsIgnoreCase(destination) && trains[i].getDepartureTime().isAfter(time)){
-                similarDestTime[i] = trains[i];
-                count++;
-            }
-        }
-        similarDestTime = Arrays.copyOf(similarDestTime, count);
-        return similarDestTime;
+    public void setTrains(Train[] trains) {
+        this.trains = trains;
     }
 
-    public Train[] similarDestinationCommon(String destination) {
-        Train[] similarDestCommon = new Train[trains.length];
-        int count = 0;
-        for(int i = 0; i < trains.length; i++) {
-            if(trains[i].getDestination().equalsIgnoreCase(destination) && trains[i].getCommonSits() != 0){
-                similarDestCommon[i] = trains[i];
-                count++;
-            }
-        }
-        similarDestCommon = Arrays.copyOf(similarDestCommon, count);
-        return similarDestCommon;
-    }
-
-    public Train trainByNumber(int number) {
-        int index = binarySearch(trains, number);
-        if(index == -1) {
-            return null;
-        } else {
-            return trains[index];
-        }
-    }
-
-    private int binarySearch(Train trainArray[], int number) {
-
-        int left = 0;
-        int right = trainArray.length - 1;
-
-        while(left < right) {
-            int middle = (left + right) / 2;
-            if(trainArray[middle].getNumber() == number) {
-                return middle;
-            } else if(trainArray[middle].getNumber() < number) {
-                left = middle + 1;
-            } else {
-                right = middle;
-            }
-        }
-        return -1;
-    }
-
-    public Train[] moreLuxSits(int givenLuxSits) {
-        Train[] moreLuxSits = new Train[trains.length];
-        int count = 0;
-        for(int i = 0; i < trains.length; i++) {
-            if(trains[i].getLuxSits() > givenLuxSits){
-                moreLuxSits[i] = trains[i];
-                count++;
-            }
-        }
-        moreLuxSits = Arrays.copyOf(moreLuxSits, count);
-        return moreLuxSits;
+    public Train[] getTrains() {
+        return trains;
     }
 }
